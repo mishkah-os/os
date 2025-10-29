ed96c8a98fe17abb09da4a7cf7701490c6c5a0f7
mishkah-os <quranaicode@gmail.com>
2025-10-29 20:49:27 +0300

    Update gym.js

--- static/projects/gym/gym.js
+++ static/projects/gym/gym.js
@@ -1,6 +1,6 @@
 import { createPosDb } from './pos/pos-mini-db.js';
-import { div, button, span, input, label, select, option, textarea } from './lib/mishkah.div.js';
-import { createSimpleStore } from './lib/mishkah.simple-store.js';
+import { div, button, span, input, label, select, option, textarea } from '../../lib/mishkah.div.js';
+import { createSimpleStore } from '../../lib/mishkah.simple-store.js';
 
 const BRANCH_ID = 'gim';
 const MODULE_ID = 'gym';
